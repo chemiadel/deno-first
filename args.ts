4e5dd8bdc01b1069d11018c233b128fb823962ec
Adel <pace3man@gmail.com>
2021-11-08 08:40:45 +0100

    finish get started

---
+++ args.ts
@@ -0,0 +1,6 @@
+//Accept url arg on shell then fetch
+const url = Deno.args[0];
+const res = await fetch(url);
+
+const body = new Uint8Array(await res.arrayBuffer());
+await Deno.stdout.write(body);
